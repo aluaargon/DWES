3c27798f569c93fe32b3ed69b4e3297bf400ac79
Aaron <alurui1562@ieselcaminas.org>
2021-09-29 09:50:10 +0200

    crate tabla

--- funcionesArrays/arrays.php
+++ funcionesArrays/arrays.php
@@ -7,16 +7,45 @@
     </head>
     <body>
         <?php
-            $nombres = ['Bea', 'Ivan', 'Cristina', 'Victor', 'Rodrigo'];
-            echo count($nombres);
+            $nombres = ['Bea', 'Ivan', 'Aaron', 'Cristina', 'Victor', 'Rodrigo'];
+            echo count($nombres) . '<br>';
             $nombresSeparadosEspacio = implode(" ", $nombres);
-            echo $nombresSeparadosEspacio;
+            echo $nombresSeparadosEspacio  . '<br>';
             asort($nombres);
-            foreach($ombres as $nombre){
+            foreach($nombres as $nombre){
                 echo $nombre . '<br>';
             }
-            $reversed = 
-
-        ?>
+            $reversed = array_reverse($nombres);
+            foreach($reversed as $reverse){
+                echo $reverse . '<br>';
+            }
+            echo "Aaron esta en la clave: " . array_search('Aaron', $nombres) . '<br>';
+            
+            $alumnos = array(
+                array("id" => "1", "nombre" => "Saul", "edad" => 20),
+                array("id" => "2", "nombre" => "Roberto", "edad" => 25),
+                array("id" => "3", "nombre" => "Sara", "edad" => 21),
+                array("id" => "4", "nombre" => "Rocio", "edad" => 18)    
+            );
+            print_r($alumnos) . '<br>';
+            
+            
+       ?>
+       <table >
+           <tr>
+               <th>id</th>
+               <th>nombre</th>
+               <th>edad</th>
+           </tr>
+           <?php 
+            foreach($alumnos as $alumno => $valor){
+                
+                $id = " <td>" . $valor["id"] ."</td>";
+                $nombre = " <td>" . $valor["nombre"] ."</td>";
+                $edad = " <td>" . $valor["edad"] ."</td>";
+                echo "<tr>" . $id . $nombre . $edad ."</tr>";
+            }
+           ?>
+       </table>
     </body>
 </html>
