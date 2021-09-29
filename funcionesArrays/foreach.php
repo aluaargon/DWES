a8f456363aad10afd97687794454e84f80f72cc4
Aaron <alurui1562@ieselcaminas.org>
2021-09-29 10:05:39 +0200

    terminado arrays.php, creado foreach.php y funcionesCadena

---
+++ funcionesArrays/foreach.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Arrays</title>
+        <meta name="author" content="Aaron"> 
+    </head>
+    <body>
+        <?php
+           ?>
+    </body>
+</html>
