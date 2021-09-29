00648e83941cfc6498e530878c37bc4581c2e998
Aaron <alurui1562@ieselcaminas.org>
2021-09-29 10:44:26 +0200

    acabado primer ejecicio cadenas

--- funcionesCadenas/cadenas.php
+++ funcionesCadenas/cadenas.php
@@ -2,11 +2,15 @@
 <html lang="es">
     <head>
         <meta charset="utf-8">
-        <title>Arrays</title>
+        <title>Cadenas</title>
         <meta name="author" content="Aaron"> 
     </head>
     <body>
         <?php
+            // Ejercicio 1
+            $nombre = $_GET["nombre"]?? "Aaron";
+            $nombreTrim = trim($nombre, '/');
+            echo "Nombre con el trim: " . $nombreTrim;
            ?>
     </body>
 </html>
