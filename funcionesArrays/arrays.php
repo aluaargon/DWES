b9bd6e9d0bd0521dc06b939f510de5aa0ebcfbf4
aargon <alurui1562@ieselcaminas.org>
2021-10-05 20:36:55 +0200

    this is a test

--- funcionesArrays/arrays.php
+++ funcionesArrays/arrays.php
@@ -2,7 +2,7 @@
 <html lang="es">
     <head>
         <meta charset="utf-8">
-        <title>Arrays</title>
+        <title>Arrays php</title>
         <meta name="author" content="Aaron"> 
     </head>
     <body>
